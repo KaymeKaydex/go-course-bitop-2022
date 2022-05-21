adc4ac8d37023202bdb17b18c04cb544fafeeec1
Alelseizor <LexaCR7@bk.ru>
2022-05-21 16:56:45 +0300

    task3

--- a.zorkin/task3/gocoursebitop_1/main.go
+++ a.zorkin/task3/gocoursebitop_1/main.go
@@ -25,14 +25,14 @@ func main() {
 		log.Println("Не получилось достать информацию из запроса")
 		os.Exit(2)
 	}
-	text_request := string(rowResp)
+	textReq := string(rowResp)
 	re := regexp.MustCompile("[0-9]+")
-	numbers_request := re.FindAllString(text_request, -1)
-	//log.Println(numbers_request) - можно посмотреть числа, которые взялись из входного json
+	numbersReq := re.FindAllString(textReq, -1)
+	//log.Println(numbersReq) - можно посмотреть числа, которые взялись из входного json
 	var sum int = 0
 	var composition int = 1
-	for _, number_request := range numbers_request {
-		number, err := strconv.Atoi(number_request) //переводим строку в число
+	for _, numbersReq := range numbersReq {
+		number, err := strconv.Atoi(numbersReq) //переводим строку в число
 		if err != nil {
 			log.Println("Не удалось перевести в число")
 			os.Exit(2)
@@ -40,13 +40,12 @@ func main() {
 		sum = sum + number
 		composition = composition * number
 	}
-	sum_str := strconv.Itoa(sum)
-	composition_str := strconv.Itoa(composition)
-	mydata := []byte(sum_str + " " + composition_str)
+	sumStr := strconv.Itoa(sum)
+	compositionStr := strconv.Itoa(composition)
+	mydata := []byte(sumStr + " " + compositionStr)
 	err = ioutil.WriteFile(os.Args[1], mydata, 0777)
 	if err != nil {
 		log.Println("Не удалось записать в файл")
 		log.Println(err)
 	}
 }
-
